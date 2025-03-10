d971f2a5c6eec96031eae9462b8ae56e35023a32
RadAd <adamgates84+github@gmail.com>
2025-03-10 14:01:02 +1100

    Don't do anything in non-interactive mode

--- CmdStartup.bat
+++ CmdStartup.bat
@@ -1,4 +1,7 @@
 @echo off
+Rem does cmd line contain "/C" and therefore will no enter interactive mode
+((echo. "%CMDCMDLINE%" | findstr /I /C:" /C ") > NUL 2> NUL) && goto :of
+
 echo.
 echo CmdStartup installed
 echo See %0
