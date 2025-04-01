3f795db209b0ea324d2ec9d1c9681a5a169f580d
RadAd <adamgates84+github@gmail.com>
2025-04-01 14:18:41 +1100

    Update CmdStartup.bat

--- CmdStartup.bat
+++ CmdStartup.bat
@@ -1,6 +1,6 @@
 @echo off
 Rem does cmd line contain "/C" and therefore will not enter interactive mode
-((echo. "%CMDCMDLINE%" | findstr /I /C:" /C ") > NUL 2> NUL) && goto :of
+((echo. "%CMDCMDLINE%" | findstr /I /C:" /C ") > NUL 2> NUL) && goto :eof
 
 echo.
 echo CmdStartup installed
